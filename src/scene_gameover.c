5f6c31519bdbfcd6df9cf29c1a105630c6c7d1e1
jjcapellan <soycape@hotmail.com>
2022-02-15 12:10:14 +0100

    Fix: Gameover no detected mouse click

--- src/scene_gameover.c
+++ src/scene_gameover.c
@@ -10,8 +10,7 @@ void scene_gameover_init()
 
 void scene_gameover_update()
 {
-    if (IsKeyDown(KEY_SPACE))
-    {
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
         scene_transition_init(SCENE_GAMEOVER, SCENE_GAMEPLAY);
     }
 }
