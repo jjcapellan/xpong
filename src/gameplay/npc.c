77e699415974c5ffd13955d141ce1f52aa21ed89
jjcapellan <soycape@hotmail.com>
2022-10-07 18:19:20 +0200

    Added: probability of npc fail

--- src/gameplay/npc.c
+++ src/gameplay/npc.c
@@ -1,6 +1,7 @@
 #include "defs.h"
 #include "gameplay.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 //
 // GLOBALS
@@ -67,7 +68,7 @@ void npc_init()
     npc_state = READY;
     npc_min_time_react = NPC_MIN_TIME_REACTION;
     npc_max_time_react = NPC_REACTION_TIME_LEVEL_1;
-    npc_time_react = npc_get_time_react();
+    npc_time_react = 0.3;
     ball_prev_vel_x = 0;
     ball_prev_vel_y = 0;
 }
@@ -118,16 +119,16 @@ void check_npc_event(float delta_time)
     }
 
     // wall bounce
-    if (ball_prev_vel_y * ball.velocity.y < 0)
+    /*if (ball_prev_vel_y * ball.velocity.y < 0)
     {
         npc_state = READY;
-    }
+    }*/
 
     // player paddle bounce
     if (ball_prev_vel_x * ball.velocity.x < 0 && ball.velocity.x > 0)
     {
         npc_state = THINKING;
-        npc_time_react = npc_get_time_react();
+        npc_time_react = npc_get_time_react(); // npc_get_time_react();
     }
 
     ball_prev_vel_x = ball.velocity.x;
@@ -144,16 +145,35 @@ float npc_ease(float percent, float x0, float x1)
 
 void npc_set_current_target()
 {
-    npc_current_target.x = ball_destination.x;
-    if (ball_destination.y < 4)
-        ball_destination.y = 4 + npc.bounds.height / 2;
-    if (ball_destination.y > (480 - 4))
-        ball_destination.y = (480 - 4) - npc.bounds.height / 2;
+    float error_p;
+    bool isError = false;
 
+    npc_current_target.x = ball_destination.x;
     npc_current_target.y = ball_destination.y;
+
     easing_x0 = npc.bounds.y;
     easing_x1 = npc_current_target.y - npc.bounds.height / 2;
-    easing_duration = abs(easing_x1 - easing_x0) / (npc_speed * 0.8);
+    error_p = ((float)abs(easing_x1 - easing_x0) / (float)SCREEN_HEIGHT) * 0.3;
+    printf("error_p: %f\n", error_p);
+    if (drand48() < error_p)
+    {
+        isError = true;
+        printf("isError: %f\n", error_p);
+    }
+    if (isError == true)
+    {
+        if (easing_x1 > easing_x0)
+        {
+            easing_x1 = npc_current_target.y - npc.bounds.height - 2 * ball.bounds.height; //(npc.bounds.height*0.75 + drand48() * 10);
+            printf("easingX1_1\n");
+        }
+        else
+        {
+            easing_x1 += (npc.bounds.height * 0.75 + drand48() * 10);
+            printf("easingX1_2\n");
+        }
+    }
+    easing_duration = (npc.bounds.x - ball.bounds.x) / ball.velocity.x;
     easing_elapsed_time = 0;
 }
 
@@ -164,7 +184,7 @@ float entity_get_y_center(Entity entity)
 
 float npc_get_time_react()
 {
-    return (drand48() * (npc_max_time_react - npc_min_time_react) + npc_min_time_react);
+    return 0.1; //(drand48() * (npc_max_time_react - npc_min_time_react) + npc_min_time_react);
 }
 
 void npc_reset()
